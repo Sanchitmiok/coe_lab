d10029b5eff2aa837ed9ae83b3031d1d5362517c
Sanchit Mishra <116272108+Sanchitmiok@users.noreply.github.com>
2023-06-16 17:58:51 +0530

    Update multiply.c

--- 2D_arr/multiply.c
+++ 2D_arr/multiply.c
@@ -1,12 +1,49 @@
 #include<stdio.h>
+
 int main(){
-    int res;
-    int arr1[2][3]={1,2,3,4,5,6};
-    int arr2[2][3]={1,2,3,4,5,6};
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-            res=arr1[i][j]*arr2[i][j];
-            printf("%d ",res);
+    int r,c;
+    printf("Enter the number of rows:");
+    scanf("%d",&r);
+
+    printf("Enter the number of columns:");
+    scanf("%d",&c);
+
+    int arr[r][c];
+
+    for (int i=0;i<r;i++){
+        for (int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
         }
     }
-}
+
+    int r1,c1;
+    printf("Enter the number of rows:");
+    scanf("%d",&r1);
+
+    printf("Enter the number of columns:");
+    scanf("%d",&c1);
+
+    int arr1[r1][c1];
+
+    for (int i=0;i<r1;i++){
+        for (int j=0;j<c1;j++){
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+
+
+    int sumarr[r][c];
+
+    for (int i=0;i<r;i++){
+        for (int j=0;j<c;j++){
+            sumarr[i][j]=arr[i][j]+arr1[i][j];
+        }
+    }
+
+    for (int i=0;i<r1;i++){
+        for (int j=0;j<c1;j++){
+            printf("%d ",sumarr[i][j]);
+        }
+
+        printf("\n");
+    }
